e5ee3831314f6e36b42f2c92c9b857794d166dca
Wa$$up <Wa$$up>
2020-02-15 22:27:30 +0300

    Bug fix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import platform
 
+this_path = os.path.dirname(os.path.abspath(__file__))
+
 # check linux systems
 
 python = 'python'
 if platform.system() != 'Windows':
     python = 'python3'
 
-os.system(python + ' MainMenu/main.py')
+path = os.path.join(this_path, 'MainMenu/main.py')
+os.system(python + ' ' + path)
